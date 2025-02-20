291a94327845f80047c4f69a145ece3464f90306
Elena Ceinos <elenaceinos@hotmail.com>
2025-02-20 14:32:00 +0100

    modified classpath
    about us working, contact not working, home either

--- backend/src/main/java/es/codeurjc/controller/DefaultController.java
+++ backend/src/main/java/es/codeurjc/controller/DefaultController.java
@@ -1,23 +1,26 @@
 package es.codeurjc.controller;
 
-// Spring
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import es.codeurjc.service.ApartmentService;
 
 @Controller
 public class DefaultController {
 
-    @Autowired
-    private ApartmentService apartmentService;
-
-    // Página de inicio - Muestra los apartamentos
     @GetMapping("/")
-    public String showIndex(Model model) {
-        model.addAttribute("apartments", apartmentService.findAll());
-        return "index"; // Sin barra "/" porque Spring busca en /templates/index.html
+    public String showIndex() {
+        return "index";  
     }
+
+    @GetMapping("/about")
+    public String showAbout() {
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String showContact() {
+        return "contact";
+    }
+
+   
+    
 }
