4a848ad2bbf0ec2cb99caf0c8e2f258418775d2c
Daniel <danigomezlopez@gmail.com>
2025-02-22 11:10:54 +0100

    Revert "Seguridad"
    This reverts commit f962497713d8559c9707a9a88c895286dc6cb7ad.

--- backend/src/main/java/es/codeurjc/security/SecurityConfiguration.java
+++ backend/src/main/java/es/codeurjc/security/SecurityConfiguration.java
@@ -50,13 +50,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.User;
-
 
 import es.codeurjc.service.UserSecurityService;
 
@@ -70,16 +66,6 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-    UserDetails user = User.builder()
-    .username("user")
-    .password(passwordEncoder().encode("pass"))
-    .roles("USER")
-    .build();
-    return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
