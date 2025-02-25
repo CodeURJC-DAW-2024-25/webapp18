5fe4738fdea49ab75f4a7bf896253624adfe1c53
Carlos Hermán <ch.andres.2020@alumnos.urjc.es>
2025-02-25 23:58:06 +0100

    Index working Index template now works properly

---
+++ backend/src/main/resources/static/js/loadApartments.js
@@ -0,0 +1,18 @@
+const NUM_RESULTS = 6;
+
+let timesRequested = 0; // it's 1 because the first 6 hotels are loaded with the /index
+
+async function loadMoreApartments(){
+
+    const start = (timesRequested) * NUM_RESULTS;
+    const end = start + NUM_RESULTS;
+    timesRequested++;
+
+    const response = await fetch(`/loadMoreApartments/${start}/${end}`);
+    const newApartments = await response.text();
+
+
+    const apartmentsDiv = document.getElementById("apartmentsList");
+    apartmentsDiv.innerHTML += newApartments;
+
+}
