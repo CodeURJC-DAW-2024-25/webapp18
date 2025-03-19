119c07fbb9a211a420e625b4f95bedf61a219856
Daniel <danigomezlopez@gmail.com>
2025-03-19 11:55:56 +0100

    Fixed review dtos and rest

--- backend/src/main/java/es/codeurjc/dto/ReviewDTO.java
+++ backend/src/main/java/es/codeurjc/dto/ReviewDTO.java
@@ -1,12 +1,68 @@
 package es.codeurjc.dto;
-
+import java.time.LocalDate;
 
 public class ReviewDTO {
-    private Long id;
-    private String user;
-    private String apartmente;
+    private Long reviewId;
+    private Long userId;
+    private Long apartmentId;
     private int score;
-    private Date date;
+    private LocalDate date;
     private String comment;
 
+
+
+
+public Long getReviewId() {
+    return reviewId;
 }
+
+public void setReviewId(Long reviewId) {
+    this.reviewId = reviewId;
+}
+
+
+public Long getUserId() {
+    return userId;
+}
+
+public void setUserId(Long userId) {
+    this.userId = userId;
+}
+
+
+public Long getApartmentId() {
+    return apartmentId;
+}
+
+public void setApartmentId(Long apartmentId) {
+    this.apartmentId = apartmentId;
+}
+
+
+public int getScore() {
+    return score;
+}
+
+public void setScore(int score) {
+    this.score = score;
+}
+
+
+public LocalDate getDate() {
+    return date;
+}
+
+public void setDate(LocalDate date) {
+    this.date = date;
+}
+
+public String getComment() {
+    return comment;
+}
+
+public void setComment(String comment) {
+    this.comment = comment;
+}
+
+}
+
--- backend/src/main/java/es/codeurjc/dto/ReviewMapper.java
+++ backend/src/main/java/es/codeurjc/dto/ReviewMapper.java
@@ -1,30 +1,23 @@
 package es.codeurjc.dto;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-import es.codeurjc.dto.ReviewBasicDTO;
-import es.codeurjc.dto.NewReviewDTO;
 import es.codeurjc.model.Review;
 
 public class ReviewMapper {
 
     public static Review toEntity(NewReviewDTO dto) {
-    Review review = new Review();
-    review.setIdReview(review.getIdReview());
-    review.setUser(review.getUser().getName());
-    review.setApartmentId(review.getApartmentId().getName());
-    review.setScore(review.getScore());
-    review.setDate(review.getDate());
-    review.setComment(review.getComment());
-    return review;
+        Review review = new Review();
+        review.setScore(dto.getScore());
+        review.setComment(dto.getComment());
+        review.setUserId(dto.getUserId());
+        review.setApartmentId(dto.getApartmentId());
+        return review;
     }
 
     public static ReviewDTO toDTO(Review review) {
-        Review dto = new Review();
-        dto.setIdReview(review.getIdReview());
-        dto.setUser(review.getUser().getName());
-        dto.setApartment(review.getApartment().getName());
+        ReviewDTO dto = new ReviewDTO(); // Ahora es un ReviewDTO
+        dto.setReviewId(review.getReviewId());
+        dto.setUserId(review.getUserId());
+        dto.setApartmentId(review.getApartmentId());
         dto.setScore(review.getScore());
         dto.setDate(review.getDate());
         dto.setComment(review.getComment());
